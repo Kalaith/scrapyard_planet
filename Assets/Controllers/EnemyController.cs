0e0c62e71832ddfcd7c930c122001b524f0ae9f9
shuttah627 <michaelbrettbarnett@gmail.com>
2017-04-26 22:15:44 +1000

    Tonights Work
    The hour or so of work done during the meeting on the 26/4.

---
+++ Assets/Controllers/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<GameObject> Enemies = new List<GameObject>();
+        Enemy example = new Enemy(1, 2,( 3, 2 );
+        Enemies.Add(example);
+        // This is all wrong. Will fix next time i work on this part.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    private int _Type;
+    private int _Health;
+    private Vector2 _SpawnPos;
+    private float _SlopeToShip;
+    public int Speed;
+
+    public Enemy(int t, int h, Vector2 s)
+    {
+        _Type = t;
+        _Health = h;
+        _SpawnPos = s;
+
+        GameObject x = new GameObject("Enemy");
+
+    }
+
+    public float CalculateSlope()
+    {
+        return _SpawnPos[1] / _SpawnPos[0];
+    }
+		
+    void Update ()
+    {
+        if (this.transform.localPosition.x > 0)
+        {
+            this.transform.Translate(-(Speed), _SlopeToShip, 0);
+        } else
+        {
+            this.transform.Translate(Speed, _SlopeToShip, 0);
+        }
+    }
+	
+}
